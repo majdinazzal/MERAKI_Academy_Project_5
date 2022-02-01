17ce5acc193c77397c1c45ad395722bc36ff082d
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-02 01:39:00 +0200

    working on getAllOrders Function

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -3,7 +3,15 @@ const db = require("../database/db");
 
 const getAllOrders=(req,res)=>{
 
+const query=`select * from orders`;
 
+connection.query(query,(err,result)=>{
+
+console.log(result);
+
+
+
+})
 
 
 
