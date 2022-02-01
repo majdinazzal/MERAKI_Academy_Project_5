2e59774037190ebd2a81a01a5ef2809d27246989
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-01 18:44:39 +0200

    users router

---
+++ backend/routes/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { createNewUser } = require("../controllers/users");
+
+
+const usersRouter = express.Router();
+
+
+usersRouter.post("/", createNewUser);
+
+module.exports = usersRouter;
