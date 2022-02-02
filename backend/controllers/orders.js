cd24ccc78149efcef26133176cedd9196ba9adfa
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-02 03:18:27 +0200

    finished addOrders function

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -26,6 +26,21 @@ const addOrders = (req, res) => {
     req.body;
   const query = `insert into orders (Product_Name, Product_Price, Product_Description, product_id) values (?,?,?,?)`;
   const data = [Product_Name, Product_Price, Product_Description, product_id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
+    } else {
+      console.log(result);
+      res.status(200).json({
+        success: true,
+        message: `${Product_Name} was added`,
+        result: result,
+      });
+    }
+  });
 };
 
 module.exports = { getAllOrders };
