1566e92b79c4caa924a7df9e425f164b34cfbdf2
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-02 12:53:04 +0200

    login

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -4,34 +4,31 @@ const jwt = require("jsonwebtoken");
 
 const login = (req, res) => {
   const password = req.body.password;
-  const email = req.body.email.toLowerCase();
-
-  const query = `SELECT * FROM users WHERE email=?`;
+  const email = req.body.email.toLowerCase(); // toLowerCase() feature
+  const query = `SELECT * FROM user WHERE email=?`;
   const data = [email];
   connection.query(query, data, (err, results) => {
-    console.log(results);
     if (err) throw err;
     if (results.length > 0) {
-      bcrypt.compare(password, results[0].password, (err, response) => {
+      bcrypt.compare(password, results[0].Password, (err, response) => {
         if (err) res.json(err);
         if (response) {
           const paylod = {
             userId: results[0].id,
             Phone_number: results[0].Phone_number,
           };
+          console.log(paylod.userId,"    ", paylod.Phone_number);
           const secret = process.env.SECRET;
 
           const token = jwt.sign(paylod, secret);
 
           res.status(200).json(token);
         } else {
-          res
-            .status(403)
-            .json({
-              success: false,
-              message: `The password you’ve entered is incorrect`,
-              err,
-            });
+          res.status(403).json({
+            success: false,
+            message: `The password you’ve entered is incorrect`,
+            err,
+          });
         }
       });
     } else {
