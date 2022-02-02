8fa3d8f6ddd17eaaa70448e868823a83cd8bcea8
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-02 02:02:51 +0200

    finished getALLOrders function

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -1,20 +1,23 @@
 const connection = require("../database/db");
 const db = require("../database/db");
 
-const getAllOrders=(req,res)=>{
+const getAllOrders = (req, res) => {
+  const query = `select * from orders`;
 
-const query=`select * from orders`;
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
+    } else {
+      console.log(result);
+      res
+        .status(200)
+        .json({ success: true, message: "All orders", result: result });
+    }
+  });
+};
 
-connection.query(query,(err,result)=>{
 
-console.log(result);
-
-
-
-})
-
-
-
-
-
-}
+//get checkout ordersF
