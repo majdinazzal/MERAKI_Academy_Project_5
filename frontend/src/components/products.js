4cc909dc69580791308be3e407a5793aafaec823
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-03 11:09:56 +0200

    working on the products component

--- frontend/src/components/products.js
+++ frontend/src/components/products.js
@@ -9,3 +9,6 @@ export const setProducts = (products) => {
 export const addProducts = (products) => {
   return { type: "ADD_PRODUCTS", payload: products };
 };
+export const updateProduct = (products) => {
+  return { type: "UPDATE_PRODUCT", payload: products };
+};
