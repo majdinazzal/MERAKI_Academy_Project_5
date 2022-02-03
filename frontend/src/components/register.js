f1c013cbf69ec5c0d6d636225e470d4251b59b76
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-02-03 11:13:19 +0200

    make function adduser

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -11,10 +11,35 @@ const Register = () => {
     });
   
     const [username, setFirstName] = useState("");
-    const [phoneNumber, setCountry] = useState(0);
+    const [phoneNumber, setphoneNumber] = useState(0);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     // const role_id = "1";
     const [message, setMessage] = useState("");
     const [status, setStatus] = useState(false);
-  
+    // =================================================================
+
+    const addNewUser = async (e) => {
+        e.preventDefault();
+        try {
+          const result = await axios.post("http://localhost:5000/users", {
+            username,
+            phoneNumber,
+            email,
+            country,
+            password,
+                     });
+          if (result.data.success) {
+            setStatus(true);
+            setMessage("The user has been created successfully");
+          } else throw Error;
+        } catch (error) {
+          setStatus(false);
+          if (error.response && error.response.data) {
+            return setMessage(error.response.data.message);
+          }
+          setMessage("Error happened while register, please try again");
+        }
+      };
+    
+    
