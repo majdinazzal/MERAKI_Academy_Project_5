80fd2d18410ff48305f19a15f1aed2343e75055d
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-02-03 11:10:53 +0200

    make state

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -5,3 +5,16 @@ import axios from "axios";
 import { useSelector} from "react-redux";
 
 // =================================================================
+const Register = () => {
+    const state = useSelector((state) => {
+      return { isLoggedIn: state.loginReducer.isLoggedIn };
+    });
+  
+    const [username, setFirstName] = useState("");
+    const [phoneNumber, setCountry] = useState(0);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    // const role_id = "1";
+    const [message, setMessage] = useState("");
+    const [status, setStatus] = useState(false);
+  
