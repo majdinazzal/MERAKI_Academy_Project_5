bc9f8b78cb90cf734e0034ba192104832b96ddc5
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-03 11:06:49 +0200

    working on the products component

---
+++ frontend/src/components/products.js
@@ -0,0 +1,11 @@
+const iniaialState = {
+  products: [],
+};
+
+export const setProducts = (products) => {
+  return { type: "SET_PRODUCTS", payload: products };
+};
+
+export const addProducts = (products) => {
+  return { type: "ADD_PRODUCTS", payload: products };
+};
