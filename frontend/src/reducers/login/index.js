bd0c868d7f70738acfb72b041df1b178037bf31e
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-02-03 12:24:30 +0200

    make action reducer

--- frontend/src/reducers/login/index.js
+++ frontend/src/reducers/login/index.js
@@ -3,3 +3,27 @@ const initialState = {
     isLoggedIn: localStorage.getItem("token") ? true : false,
   };
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//action
+  export const login = (token) => {
+    return { type: "LOG_IN", payload: token };
+  };
+  
+  export const logout = () => {
+    return { type: "LOG_OUT" };
+  };
+  
