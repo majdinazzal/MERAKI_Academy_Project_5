082b4697b714d4359b08d75d91ce4dad7f1c680e
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-02-03 11:05:25 +0200

    import

---
+++ frontend/src/components/register.js
@@ -0,0 +1,7 @@
+import React, { useContext, useState } from "react";
+
+import axios from "axios";
+
+import { useSelector} from "react-redux";
+
+// =================================================================
