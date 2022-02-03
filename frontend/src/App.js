8695d5995689357bd5ccaa59e8c59ca8b038b66c
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-03 18:19:01 +0200

    products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 // import components
-import Hero from "./components/hero"
-import Home from "./components/home"
-import Login from "./components/login"
-
+import Hero from "./components/hero";
+import Home from "./components/home";
+import Login from "./components/login";
+import AllProduct from "./components/products";
 
 function App() {
   return (
     <div className="App">
-     <Routes>
-      <Route path="/" element = {<Hero/>} />
-      <Route path="/home" element = {<Home/>} />
-      <Route path="/login" element = {<Login/>} />
-     </Routes>
+      <Routes>
+        <Route path="/Product" element={<AllProduct />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
