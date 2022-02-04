393b5cf5a95f4b0c104d99cb1f3f4b4849365c91
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-05 01:50:35 +0200

    updating the DB

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,37 +1,37 @@
-create database MERAKI_Academy_Project_5;
+-- create database MERAKI_Academy_Project_5;
 use MERAKI_Academy_Project_5;
 
 
-create table user (
-id INT AUTO_INCREMENT NOT NULL,
-Username VARchar (255) UNIQUE,
-Phone_number INT,
-email VARchar (255) UNIQUE,
-Password VARchar (255),
-Primary Key (id)
-);
---  user name check??
+-- create table user (
+-- id INT AUTO_INCREMENT NOT NULL,
+-- Username VARchar (255) UNIQUE,
+-- Phone_number INT,
+-- email VARchar (255) UNIQUE,
+-- Password VARchar (255),
+-- Primary Key (id)
+-- );
+-- --  user name check??
 
 
 
-create table products (
-id INT AUTO_INCREMENT NOT NULL,
-Product_Name VARchar (255),
-Description VARchar (255),
-userId INT,
-Price INT,
-Category VARchar (255),
-FOREIGN KEY (userId) REFERENCES user(id),
-Primary Key (id)
-);
+-- create table products (
+-- id INT AUTO_INCREMENT NOT NULL,
+-- Product_Name VARchar (255),
+-- Description VARchar (255),
+-- userId INT,
+-- Price INT,
+-- Category VARchar (255),
+-- FOREIGN KEY (userId) REFERENCES user(id),
+-- Primary Key (id)
+-- );
 
-create table orders (
-id INT AUTO_INCREMENT NOT NULL,
-Product_Name VARCHAR (255),
-Product_Price INT,
-Product_Description VARCHAR (255),
-product_id INT,
-FOREIGN KEY (product_id) REFERENCES products(id),
-Primary Key (id)
-);
+-- create table orders (
+-- id INT AUTO_INCREMENT NOT NULL,
+-- Product_Name VARCHAR (255),
+-- Product_Price INT,
+-- Product_Description VARCHAR (255),
+-- product_id INT,
+-- FOREIGN KEY (product_id) REFERENCES products(id),
+-- Primary Key (id)
+-- );
 
