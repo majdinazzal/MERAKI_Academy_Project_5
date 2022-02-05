13dd8564c763c53d06515b6a6c59272675d56f2f
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-06 01:25:06 +0200

    still working on the search BE

--- backend/controllers/search.js
+++ backend/controllers/search.js
@@ -2,6 +2,11 @@ const connection = require("../database/db");
 const db = require("../database/db");
 
 const search = (req, res) => {
-
+    const Product_Name = req.params.Product_Name;
+    const query = `select * from products where Product_Name =${Product_Name}`;
+    const data = [Product_Name];
+    connection.query(query, data, (err, result) => {
+        
+    });
 };
 module.exports = { search };
