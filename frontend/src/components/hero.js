1de36cfb1b25b8cd729ef38e4ebb69518d73ef65
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-05 16:49:02 +0200

    hero styling

--- frontend/src/components/hero.js
+++ frontend/src/components/hero.js
@@ -5,17 +5,24 @@ const Hero = ()=> {
 return (
 <>
     <p>This is the Hero Page</p>
-    <div className="NavBar" display="inline" color="blue">
-<ul className="ul" >
-      <li className="list" ><Link className="list" to="/home"><a className="">Home</a></Link>{" "}
-      </li> 
-      <li className="list" ><Link className="list" to="/login"><a className="">Login</a></Link>{" "}
-      </li> 
-</ul>
+<div className="logo">
+    <h2>Xchange</h2>
+</div>
+    <div className="NavBar">
+      <div ><Link to="/home"><a className="">Home</a></Link>{" "}
+      </div> 
+      <div><Link to="/login"><a className="">Login</a></Link>{" "}
+      </div> 
     </div>
     <div className="body">
-        <img/>
-    </div>
+        <div></div>
+        <div></div>
+        <div className="icons">
+        <div>First Register to have an account</div>
+        <div>Second Login to your account</div>
+        <div>Third Start Item Exchanging</div>
+        </div>
+            </div>
     <div className="Footer">
         <p>Meraki C4 </p>
         <p>Project Done By team A4 </p>
