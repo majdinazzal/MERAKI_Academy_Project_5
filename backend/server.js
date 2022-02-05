94d3cf3f97e3825768b79d3cdabe5a338677cbf6
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-06 01:40:42 +0200

    searchRouter

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,17 @@ app.use(express.json());
 const PORT = 5000;
 
 // Import Routers
+const searchRouter = require("./routes/searchR");
 const usersRouter = require("./routes/users");
 const loginRouter = require("./routes/login");
 const ordersRouter = require("./routes/orders");
 const productRouter = require("./routes/product");
 // Routes Middleware
 app.use("/users", usersRouter);
-app.use("/login",  loginRouter);
+app.use("/login", loginRouter);
 app.use("/orders", ordersRouter);
 app.use("/product", productRouter);
+app.use("/search", searchRouter);
 
 app.listen(PORT, () => {
   console.log(`SERVER WORKING ON PORT: ${PORT}`);
