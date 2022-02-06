cbaf44a53c526c2ad8d7b1de7c8ee8cc8e03abff
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-02-06 11:56:51 +0200

    logincss2

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 
 import axios from "axios";
-
+import "./login.css"
 import { useSelector } from "react-redux";
 
 // =================================================================
