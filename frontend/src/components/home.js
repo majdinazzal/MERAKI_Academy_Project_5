7d36f2c162f9970f0c96d1cc1306fa21c63f3a93
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-06 02:13:27 +0200

    added the search bar to the home but need to fix error

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -3,7 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { setProducts } from "../reducers/products/index";
-import "./home.css"
+import "./home.css";
 import { Link } from "react-router-dom";
 const Home = () => {
   const state = useSelector((state) => {
@@ -18,10 +18,24 @@ const Home = () => {
   // ---------------------------------------------
   const [show, setShow] = useState(false);
   const [productsShower, setProductsShower] = useState([]);
+  const [Product_Name, setProduct_Name] = useState("");
+  const [found, setFound] = useState([]);
 
   //===============================================================
-//hi
-
+  //hi
+  const searchFunc = () => {
+    console.log("inside search");
+    axios
+      .get(`http://localhost:5000/search/${Product_Name}`)
+      .then((result) => {
+        console.log(result.data);
+        setFound(result.data.posts);
+      })
+      .catch((err) => {
+        console.log(err.response);
+        console.log(err);
+      });
+  };
   const allProducts = async () => {
     try {
       const res = await axios.get("http://localhost:5000/product");
@@ -40,14 +54,57 @@ const Home = () => {
   return (
     <div>
       <div className="logo">
-    <h2>Xchange</h2>
-</div>
-<div className="NavBar">
-      <div ><Link to="/home"><a className="">Home</a></Link>{" "}
-      </div> 
-      <div><Link to="/login"><a className="">Login</a></Link>{" "}
-      </div> 
-    </div>
+        <h2>Xchange</h2>
+      </div>
+      <div className="NavBar">
+        <div>
+          <Link to="/home">
+            <a className="">Home</a>
+          </Link>{" "}
+        </div>
+        <div>
+          <Link to="/login">
+            <a className="">Login</a>
+          </Link>{" "}
+        </div>
+        <div>
+          <label>
+            <input
+              placeholder="Search"
+              type={"text"}
+              onChange={(e) => {
+                console.log(e);
+                setProduct_Name(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                searchFunc();
+                console.log(found);
+              }}
+            >
+              Search
+            </button>
+          </label>
+          {found.length ? (
+            //length is not defined???
+            <div>
+              {found &&
+                found.map((elem) => {
+                  return (
+                    <div>
+                      <h2>{elem.title}</h2>
+                      <p>{elem.description}</p>
+                      <h6>{elem.category}</h6>
+                    </div>
+                  );
+                })}
+            </div>
+          ) : (
+            <p></p>
+          )}
+        </div>
+      </div>
       <div className="product">
         {" "}
         {productsShower &&
@@ -65,7 +122,7 @@ const Home = () => {
         <div>Meraki C4 </div>
         <div>Project Done By team A4 </div>
         <div> c 2022 </div>
-    </div>
+      </div>
     </div>
   );
 };
