0547124dc3fdcd9bb536a4460b91b2a9cc2cb64e
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-08 21:54:49 +0200

    footer add

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,5 +1,6 @@
 import "./footer.css";
-const footer = ()=>{
+
+const Footer = ()=>{
 
 return(
     <>
@@ -8,9 +9,19 @@ return(
         <div>Project Done By team A4 </div>
         <div> c 2022 </div>
       </div>
+      <div className="Footer">
+        <div>Mercedes </div>
+        <div>Audi </div>
+        <div> Toyota </div>
+        <div> Land Rover </div>
+        <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/1200px-BMW_logo_%28gray%29.svg.png"/> BMW </div>
+        <div> Cadillac </div>
+        <div> Jaguar </div>
+      </div>
+  
 </>
 )
 }
 
-export default footer;
+export default Footer;
 
