ec7fc79435b6f52780e01f2ef20eaf49af8eb06d
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-08 20:31:21 +0200

    footer

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import "./footer.css";
+const footer = ()=>{
+
+return(
+    <>
+<div className="Footer">
+        <div>Meraki C4 </div>
+        <div>Project Done By team A4 </div>
+        <div> c 2022 </div>
+      </div>
+</>
+)
+}
+
+export default footer;
+
