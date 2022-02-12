c550eb576969277fb66aa189d3b5d614367ed492
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-12 16:19:01 +0200

    user information function

--- backend/controllers/profileBE.js
+++ backend/controllers/profileBE.js
@@ -1,7 +1,22 @@
+const connection = require("../database/db");
 const db = require("../database/db");
 
 const profile = (req, res) => {
-  const { Username } = req.params.Username;
-  const query = `select user`;
+  const userId = req.params.userId;
+  const query = `select Username,Phone_number,email from user where id=${userId}`;
+  const data = [userId];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
+    } else {
+      console.log(result);
+      res
+        .status(200)
+        .json({ success: true, message: "User Information", result: result });
+    }
+  });
 };
 module.exports = profile;
