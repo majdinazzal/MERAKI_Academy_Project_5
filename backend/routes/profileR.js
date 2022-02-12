d8e02eee590f24f9946873cde30470868b9cff7d
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-02-12 19:13:24 +0200

    working on the profile page

--- backend/routes/profileR.js
+++ backend/routes/profileR.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const profile = require("../controllers/profileBE");
+const { profile, profileImage } = require("../controllers/profileBE");
 const profileRouter = express.Router();
 //main route is /profile
 profileRouter.get("/:userId", profile);
+profileRouter.put("/:userId", profileImage);
 module.exports = profileRouter;
