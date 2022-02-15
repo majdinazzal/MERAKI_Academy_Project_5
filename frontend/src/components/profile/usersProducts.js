0ce319126538df259c3b58396f09329293399208
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-15 23:32:52 +0200

    profile component

---
+++ frontend/src/components/profile/usersProducts.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Modal from "react-modal/lib/components/Modal";
+import UserProducts from "./userProductRender";
+import "./profile.css"
+
+
+const ProfileModal = () => {
+  const [modalShower, setModalShower] = useState(false);
+  const switchModalTrue = () => {
+    setModalShower(true);
+  };
+  const switchModalfalse = () => {
+    setModalShower(false);
+  };
+  return (
+    <div>
+      <br/>
+      <button className="showbutton" onClick={switchModalTrue}>Show your products</button>
+      <Modal isOpen={modalShower}>
+        <br/>
+        <button className="profilebutton" onClick={switchModalfalse}>close</button>
+        <br />
+        <UserProducts />{" "}
+      </Modal>
+    </div>
+  );
+};
+export default ProfileModal;
