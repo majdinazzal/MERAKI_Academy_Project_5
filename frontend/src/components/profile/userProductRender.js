0580eb9ffef218be2e4f8292d670cdee11c121ee
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-15 23:32:44 +0200

    profile component

---
+++ frontend/src/components/profile/userProductRender.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import "./profile.css"
+const UserProducts = () => {
+  const [allPrdcts, setAllPrdcts] = useState([]);
+  const userId = localStorage.getItem("User");
+  const getUsersProducts = () => {
+    axios
+      .get(`http://localhost:5000/product/byuser/${userId}`)
+      .then((result) => {
+        console.log(result);
+        setAllPrdcts(result.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(getUsersProducts, []);
+
+  return (
+    <div>
+      {/* <h1>afsf</h1> */}
+      {allPrdcts &&
+        allPrdcts.map((elem, i) => {
+          return (
+            <div key={i}>
+              {" "}
+              <br />
+              product name : {elem.Product_Name} <br />
+              product state : ({elem.state_product}) <br />
+              <button className="profilebutton"
+                onClick={() => {
+                  Swal.fire("description :", elem.Description, elem.Price);
+                }}
+              >
+                more
+              </button>
+              <br />
+              =====================================
+            </div>
+            // flex display then flex then gap between the render and the button
+          );
+        })}{" "}
+    </div>
+  );
+};
+export default UserProducts;
