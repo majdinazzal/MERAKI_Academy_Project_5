af330509fc9024873872289c08ac9b8d3074a789
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-17 13:36:49 +0200

    deploys1

--- index.js.js
+++ index.js.js
@@ -7,7 +7,14 @@ const db = require("./database/db");
 app.use(cors());
 
 app.use(express.json());
+const path = require("path");
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 const PORT = 5000;
 
 // Import Routers
