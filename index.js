02c8983e570d231ce1fadb4955da855cb1e5d5cb
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-18 01:01:21 +0200

    port

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const db = require("./database/db");
-
+const path = require("path");
 app.use(cors());
 
 app.use(express.json());
-const path = require("path");
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static('client/build'));
+// }
+// app.get('*',(req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
+
+app.use(express.static(path.resolve(__dirname, ".client/build")));
+
 const PORT = 5000;
 
 // Import Routers
