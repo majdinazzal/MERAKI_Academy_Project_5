f582c036484d9c2a8fb574d1f9fa240fbe661410
majdinazzal <majdiahmadnazzal@gmail.com>
2022-02-18 01:07:25 +0200

    port

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ app.use(cors());
 app.use(express.json());
 
 
-// if(process.env.NODE_ENV === 'production'){
-//   app.use(express.static('client/build'));
-// }
-// app.get('*',(req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
-app.use(express.static(path.resolve(__dirname, ".client/build")));
+
 
 const PORT = 5000;
 
